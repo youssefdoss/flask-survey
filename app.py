3bebc48b632cff1b5cdf205ed9036037f5c3d383
Chalon Lubin <chalonlubin1@gmail.com>
2022-12-01 16:06:04 -0800

    after code review

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 responses = []
-curr_question_number = None
+
 
 @app.get('/')
 def show_home_page():
@@ -20,41 +20,45 @@ def show_home_page():
         title=survey.title,
         instructions=survey.instructions)
 
+
 @app.post('/begin')
 def start_survey():
     '''Redirects user to first survey question page'''
-    
+
     return redirect('/questions/0')
+
 
 @app.get('/questions/<question_number>')
 def show_first_question(question_number):
     '''Brings the user to the first survey question page'''
-    question_number = int(question_number)
-    global curr_question_number
-    curr_question_number = question_number
+    question_number = len(responses)
 
     return render_template(
         'question.html',
         prompt=survey.questions[question_number].prompt,
         choices=survey.questions[question_number].choices)
 
+
 @app.post('/answer')
 def handle_answer():
     '''Adds answer to responses list and redirects to next question,
     or sends the user to the thank you page'''
-    global curr_question_number
-    curr_question_number += 1
 
-    if curr_question_number < len(survey.questions):
-        responses.append(request.form['answer'])
+    responses.append(request.form['answer'])
 
-        return redirect(f'/questions/{curr_question_number}')
+    if len(responses) < len(survey.questions):
+        return redirect(f'/questions/{len(responses)}')
 
     else:
         return redirect('/thank_you')
 
+
 @app.get('/thank_you')
 def show_completed_page():
     '''Displays the completion page with questions and answers'''
+    questions = survey.questions
 
-    return render_template('completion.html')
+    return render_template(
+        'completion.html',
+        questions=questions,
+        responses=responses)
